81cae328517c7db000c0144b4bfafa70810e8409
davidmertenjones <davidmertenjones@gmail.com>
2025-10-04 16:25:22 -0700

    Added comments in init_db.py, renamed 'birth_friendly' to 'maternity', changed Y/N to 1/0

--- database_setup/init_db.py
+++ database_setup/init_db.py
@@ -3,7 +3,9 @@ import pandas as pd
 import os.path
 import sqlalchemy
 
-"""connection = sqlite3.connect('database.db')
+#Hard-Coded prior to writing `csv_to_db()` function
+"""
+connection = sqlite3.connect('database.db')
 
 df = pd.read_csv('LACOUNTY.csv')
 with open('schema.sql') as f:
@@ -18,24 +20,34 @@ for i in range(len(df)):
                 )
 
 connection.commit()
-connection.close()"""
+connection.close()
+"""
 
-
+#`csv_to_db()` still requires specific schema, but writes csv to db
 def csv_to_db(csv_file, db_name, schema):
     """Takes csv file and appends each """
 
+    #Check if database already in file directory
     if os.path.isfile(db_name):
         return "Database Already Exists"
+    
+    #If not, create databse, write csv rows to it
     else:
 
+        #Open  connection to empty database file
         connection = sqlite3.connect(db_name)
 
+        #Read in csv through pandas
         df = pd.read_csv(csv_file, index_col=0)
+
+        #Convert to string to avoid int(64) to BLOB conversion; this will need refactoring later
         for column in df:
             df[column] = df[column].apply(str)
         with open(schema) as f:
             connection.executescript(f.read())
 
+
+        #Generalized column/values inputs to be placed in SQL query template
         columns = ', '.join(df.columns)
         values = ', '.join(['?']*len(df.columns))
 
@@ -43,12 +55,16 @@ def csv_to_db(csv_file, db_name, schema):
 
             cur = connection.cursor()
             #print(tuple(df.iloc[i]))
+            #Create a SQL query based on template plus columns, values, and row
             cur.execute(f"INSERT INTO lacounty ({columns}) VALUES ({values})",
                         tuple(df.iloc[i])
                         )
 
+        #Commit changes to database table
         connection.commit()
+        #Close the connection
         connection.close()
+        #Confirm process is finished
         return "Database Initialized"
 
 
@@ -64,21 +80,36 @@ def db_check(csv_file, db_name, table_name):
     cursor = con.cursor()
     cursor.execute("SELECT * FROM lacounty")
 
+    print('Queries Succeeded')
+
+    #Create database connection with SQLAlchemy
     dbEngine=sqlalchemy.create_engine('sqlite:///'+db_name)
 
-    #Old DataFrame from CSV file
+    #Open old DataFrame from CSV file
     df = pd.read_csv(csv_file, index_col=0)
+    #If run as standalone file, init_db will print out table data types
+    print(df.info())
 
-    #DataFrame from newly-created SQL database
+    #Open new DataFrame from newly-created SQL database
     df2 = pd.read_sql(f'select * from {table_name}', dbEngine)
+    #If run as standalone file, init_db will print out table data types
+    print(df2.info())
 
+    #Check if columns are the same *when cast as string objects*
+    for column in df2.columns:
+        df2[column] = df2[column].apply(str)
     for column in df.columns:
         df[column] = df[column].apply(str)
 
+    #for column in df.columns:
+    #    print(df[column].equals(df2[column]))
+
+    #Confirm that the data tables are the same:
     return df.equals(df2)
-
-
+    
 
 if __name__ == '__main__':
+
     csv_to_db('LACOUNTY.csv', 'database.db', 'schema.sql')
+
     print(db_check('LACOUNTY.csv', 'database.db', 'lacounty'))
