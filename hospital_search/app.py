171c4f8fc08c76eed259391a31ca5500ceb2b23b
davidmertenjones <davidmertenjones@gmail.com>
2025-11-11 20:11:04 -0800

    added flask_security

--- hospital_search/app.py
+++ hospital_search/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, url_for, redirect, flash, request
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_security import roles_accepted
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import InputRequired, Length, ValidationError, Email
@@ -217,6 +218,7 @@ def dashboard():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
 
 def admin_panel():
     pass
