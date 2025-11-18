9e01885e571a2bfaaaef93006d49d5b72fa5f79a
davidmertenjones <davidmertenjones@gmail.com>
2025-11-17 20:39:03 -0800

    yet more cleanup

--- hospital_search/init_db.py
+++ hospital_search/init_db.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 #from flask_login import UserMixin
-from flask_security import Security, RoleMixin, UserMixin, hash_password, SQLAlchemySessionUserDatastore
+from flask_security import Security, RoleMixin, UserMixin, SQLAlchemySessionUserDatastore
 import csv
 from flask_bcrypt import Bcrypt
 import uuid
